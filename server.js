d5fe4be5b7a83cad4b44a7e5747860ec4162696f
Erkinbek Abdurakhman <erkinbek.abdurahman2@gmail.com>
2020-10-10 23:55:49 +0600

    Switching api

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json({ extended: false }));
 // Allowing All Methods
 // Requesting Content-Type
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Origin', 'https://autodor.kg'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type');
     next();
 })
 app.get('/', (req, res) => res.send({ msg: "Avtostart Dordoi API Documentation" }));
