e0827ef248de9983d37a1cacdbb2856c2c442f65
Erkinbek Abdurakhman <erkinbek.abdurahman2@gmail.com>
2020-10-13 23:25:19 +0600

    Fixing Error from Backend

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json({ extended: false }));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 })
 app.get('/', (req, res) => res.send({ msg: "Avtostart Dordoi API Documentation" }));
